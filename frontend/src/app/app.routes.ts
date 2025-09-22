af9ebed9631b95db9b7b1a41e40c3b041e2d1ff8
iori-sutani <io.sutani@gmail.som>
2025-09-22 16:52:15 +0900

    ポスト画面のルーティングとcssの修正

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
-
 import { Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+	{ path: '', component: HomeComponent },
 	{ path: 'post', component: PostComponent },
 ];
