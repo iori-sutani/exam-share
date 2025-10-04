281cb1bc7a206b39787e46ac210283943763c05c
iori-sutani <io.sutani@gmail.som>
2025-10-05 00:45:40 +0900

    firebaseの設定を追加

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+import { firebaseConfig } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(), // Hydration を使っているなら
+    provideFirebaseApp(() => initializeApp(firebaseConfig))
     // 他のクライアント側プロバイダ...
   ],
 };
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGg3CcJYsPV6rgGs8Bo9dYXhZcuhb_FoA",
+  authDomain: "exam-share-app.firebaseapp.com",
+  projectId: "exam-share-app",
+  storageBucket: "exam-share-app.firebasestorage.app",
+  messagingSenderId: "1019519976921",
+  appId: "1:1019519976921:web:b76212e8ff6360fb9745c8",
+  measurementId: "G-BRQFMZ5YBV"
+};
