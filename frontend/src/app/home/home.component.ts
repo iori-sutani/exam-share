8e1557a075e6c772bd1bdef463f4caa3fc2ed96e
iori-sutani <io.sutani@gmail.som>
2025-10-12 13:12:17 +0900

    firebaseからデータをとってhome画面に表示

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private firestore = inject(Firestore);
+  posts$: Observable<any[]> = new Observable(); // Firestoreからの投稿データを格納
+  selectedPost: any = null; // 詳細表示用の選択された投稿データ
+
+  constructor() {}
+
+  ngOnInit() {
+    const postsCollection = collection(this.firestore, 'posts');
+    this.posts$ = collectionData(postsCollection);
+
+    this.posts$.subscribe((data) => {
+      console.log('Firestoreから取得したデータ:', data);
+    });
+  }
+
+  // 詳細画面を表示
+  showDetails(post: any): void {
+    this.selectedPost = post;
+  }
+
+  // 詳細画面を閉じる
+  closeDetails(): void {
+    this.selectedPost = null;
+  }
+}
